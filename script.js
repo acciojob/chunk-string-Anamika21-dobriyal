525eb738b803f4e5cc720f399e9efa26bd79efa8
Anamika <anamikadobriyal21@gmail.com>
2024-08-28 00:19:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function stringChop(str, size) {
+function stringChop(str,chunkLength) {
   // your code here
+	if (!str) {
+        return [];
+    }
+
+    const chunks = [];
+    // Iterate through the string, incrementing by chunkLength each time
+    for (let i = 0; i < str.length; i += chunkLength) {
+        // Push each chunk into the chunks array
+        chunks.push(str.substring(i, i + chunkLength));
+    }
+
+    return chunks;
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+alert(stringChop(str, chunkLength));
